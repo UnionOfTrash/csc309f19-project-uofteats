d95e294f7ffc298eb2d4803caa976f7f0fe8d6fd
zhang <yil.zhang@mail.utoronto.ca>
2019-10-31 15:51:24 -0400

    implement search in customer main page

---
+++ src/Data/TrackData.js
@@ -0,0 +1,28 @@
+import TruckImg from "../customer/images/truck1.png";
+
+const trackData = [
+    {
+        id: 1,
+        name: "truck 1",
+        location: "10 st George street",
+        type: "Asian",
+        rate: 4.0,
+        image: TruckImg,
+        alternative: "truck 1",
+        foodPage: "../food_page/FoodPage"
+    },
+    {
+        id: 2,
+        name: "truck 2",
+        location: "10 st George street",
+        type: "Asian",
+        rate: 4.0,
+        image: TruckImg,
+        alternative: "truck 2",
+        foodPage: "../food_page/FoodPage"
+    }
+];
+
+
+
+export default trackData

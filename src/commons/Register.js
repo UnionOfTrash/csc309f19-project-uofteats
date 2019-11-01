c5ad20322c1834225ec3a2b777063e25e066ede4
Even Zhou <even.y.zhou@outlook.com>
2019-11-01 02:07:51 -0400

    rewrite login, register and reset with antd
    in order to maintain a consistent view

--- src/commons/Register.js
+++ src/commons/Register.js
@@ -1,80 +1,86 @@
 import React from "react";
 
-import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
-import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormLabel from "@material-ui/core/FormLabel";
-import Grid from "@material-ui/core/Grid";
-import Link from "@material-ui/core/Link";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
+import "antd/dist/antd.css";
+import "./commons.css";
 
-import { withStyles } from "@material-ui/core/styles";
+import { Layout } from "antd";
+import { Row, Col } from "antd";
+import { Typography } from "antd";
+import { Form, Input, Icon, Radio, Button, message } from "antd";
 
-
-const styles = theme => ({
-    registerContainer: {
-        margin: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    registerForm: {
-        marginTop: theme.spacing(3),
-        width: '100%',
-    },
-    registerComponent: {
-        marginTop: theme.spacing(2),
-    },
-    gridComponent: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+const { Content } = Layout;
+const { Title, Text } = Typography;
 
 
 class Register extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.warned = true;
+    }
+
+    truckWarning = () => {
+        if (this.warned) {
+            this.warned = false;
+            message.warning('Well, you are not allowed to sign up as a truck manager. Please contact Mr. Edison Shi at ed.shi@mail.utoronto.ca if you are interested.', 10);
+            setTimeout(() => (this.warned = true), 10000);
+        }
+    }
+
     render() {
-        const { classes } = this.props;
 
         return (
-            <Container component='main' maxWidth='sm'>
-                <div className={ classes.registerContainer } >
-                    <Typography component='h1' variant='h5'>Get U a ticket to UofTEats!</Typography>
-                    <FormControl className={ classes.registerForm } component='form'>
-                        <TextField className={ classes.registerComponent } label='Username' type='text' variant='outlined' required fullWidth autoFocus />
-                        <TextField className={ classes.registerComponent } label='Email Address' type='email' variant='outlined' required fullWidth/>
-                        <TextField className={ classes.registerComponent } label='Password' type='password' variant='outlined' required fullWidth />
-                        <TextField className={ classes.registerComponent } label='Confirm Password' type='password' variant='outlined' required fullWidth />
-                        <Grid className={ classes.registerComponent } container>
-                            <Grid className={ classes.gridComponent } item xs={ 3 }>
-                                <FormLabel required>Role</FormLabel>
-                            </Grid>
-                            <Grid item xs>
-                                <RadioGroup row>
-                                    <FormControlLabel name='loginAsStudent' value='Student' control= { <Radio color='primary' /> } label='Student' />
-                                    <FormControlLabel name='loginAsTruck' value='Truck' control= { <Radio color='primary' /> } label='Truck Manager' disabled/>
-                                </RadioGroup>
-                            </Grid>
-                        </Grid>
-                        <Button className={ classes.registerComponent } variant='contained' color='secondary' fullWidth>Register</Button>
-                        <Grid className={ classes.registerComponent } container>
-                            <Grid className={ classes.gridComponent } item xs>
-                                <Link variant='body2' href='/login'>Already have an account? Login here!</Link>
-                            </Grid>
-                        </Grid>
-                    </FormControl>
-                </div>
-            </Container>
+            <Layout className='commonLayout'>
+                <Content>
+                    <Row>
+                        <Col xs={ 2 } sm={ 3 } md={ 6 } lg={ 7 } xl={ 8 } xxl={ 8 } />
+                        <Col xs={ 20 } sm={ 18 } md={ 12 } lg={ 10 } xl={ 8 } xxl={ 8 } className='commonContainer'>
+                            <Title level={ 4 }> Get U a ticket to UofTEats </Title>
+                            <Form className='commonForm'>
+                                <Form.Item className='commonItem'>
+                                    <Input prefix={ <Icon type='user' /> } type='text' placeholder='Username' autoFocus />
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Input prefix={ <Icon type='mail' /> } type='email' placeholder='Email Address' />
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Input.Password prefix={ <Icon type='lock' /> } type='password' placeholder='Password' />
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Input.Password prefix={ <Icon type='unlock' /> } type='password' placeholder='Confirm Password' />
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Row>
+                                        <Col span={ 8 } className='commonGridComponent'>
+                                            <Text strong> Role? </Text>
+                                        </Col>
+                                        <Col span={ 16 } className='commonGridComponent'>
+                                            <Radio.Group value='Student'>
+                                                <Radio value='Student'> Student </Radio>
+                                                <Radio value='Truck' onMouseEnter={ this.truckWarning } disabled> Truck Manager </Radio>
+                                            </Radio.Group>
+                                        </Col>
+                                    </Row>
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Button type='danger' className='commonButton'> Sign Up </Button>
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Row>
+                                        <Col span={ 24 } className='commonGridComponent'>
+                                            <Button type='link' href='/login'> Already have an account? Login Here! </Button>
+                                        </Col>
+                                    </Row>
+                                </Form.Item>
+                            </Form>
+                        </Col>
+                        <Col xs={ 2 } sm={ 3 } md={ 6 } lg={ 7 } xl={ 8 } xxl={ 8 } />
+                    </Row>
+                </Content>
+            </Layout>
         );
     }
 }
 
 
-export default withStyles(styles, {withTheme: true})(Register);
+export default Register;
--- src/commons/Reset.js
+++ src/commons/Reset.js
@@ -1,75 +1,70 @@
 import React from "react";
 
-import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
-import FormControl from "@material-ui/core/FormControl";
-import Grid from "@material-ui/core/Grid";
-import Link from "@material-ui/core/Link";
-import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
+import "antd/dist/antd.css";
+import "./commons.css";
 
-import { withStyles } from "@material-ui/core/styles";
+import { Layout } from "antd";
+import { Row, Col } from "antd";
+import { Typography } from "antd";
+import { Form, Input, Icon, Button } from "antd";
 
-
-const styles = theme => ({
-    resetContainer: {
-        margin: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    resetForm: {
-        marginTop: theme.spacing(3),
-        width: '100%',
-    },
-    resetComponent: {
-        marginTop: theme.spacing(2),
-    },
-    gridComponent: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+const { Content } = Layout;
+const { Title } = Typography;
 
 
 class Reset extends React.Component {
 
     render() {
-        const { classes } = this.props;
 
         return (
-            <Container component='main' maxWidth='sm'>
-                <div className={ classes.resetContainer } >
-                    <Typography component='h1' variant='h5'>Reset your password here</Typography>
-                    <FormControl className={ classes.resetForm } component='form'>
-                        <Grid className={ classes.resetComponent} container>
-                            <Grid className={ classes.gridComponent } item xs={ 9 } >
-                                <TextField label='Email Address' type='email' variant='outlined' required fullWidth/>
-                            </Grid>
-                            <Grid className={ classes.gridComponent } item xs>
-                                <Button variant='contained' color='primary'>Send Code</Button>
-                            </Grid>
-                        </Grid>
-                        <TextField className={ classes.resetComponent } label='Code' type='text' variant='outlined' required fullWidth/>
-                        <TextField className={ classes.resetComponent } label='New Password' type='password' variant='outlined' required fullWidth disabled/>
-                        <TextField className={ classes.resetComponent } label='Confirm New Password' type='password' variant='outlined' required fullWidth disabled/>
-                        <Button className={ classes.resetComponent } variant='contained' color='secondary' fullWidth>Reset</Button>
-                        <Grid className={ classes.resetComponent } container>
-                            <Grid className={ classes.gridComponent } item xs>
-                                <Link variant='body2' href='/login'>Login here!</Link>
-                            </Grid>
-                            <Grid className={ classes.gridComponent } item xs>
-                                <Link variant='body2' href='/register'>Or register here!</Link>
-                            </Grid>
-                        </Grid>
-                    </FormControl>
-                </div>
-            </Container>
+            <Layout className='commonLayout'>
+                <Content>
+                    <Row>
+                        <Col xs={ 2 } sm={ 3 } md={ 6 } lg={ 7 } xl={ 8 } xxl={ 8 } />
+                        <Col xs={ 20 } sm={ 18 } md={ 12 } lg={ 10 } xl={ 8 } xxl={ 8 } className='commonContainer'>
+                            <Title level={ 4 }> Reset your password </Title>
+                            <Form className='commonForm'>
+                                <Form.Item className='commonItem'>
+                                    <Row gutter={ 1 }>
+                                        <Col span={ 20 }>
+                                            <Input prefix={ <Icon type='user' /> } type='email' placeholder='Email Address' autoFocus />
+                                        </Col>
+                                        <Col span={ 4 }>
+                                            <Button color='primary' className='commonButton'> Send Code </Button>
+                                        </Col>
+                                    </Row>
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Input prefix={ <Icon type='number' /> } type='text' placeholder='Code' />
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Input.Password prefix={ <Icon type='lock' /> } type='password' placeholder='New Password' />
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Input.Password prefix={ <Icon type='unlock' /> } type='password' placeholder='Confirm New Password' />
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Button type='danger' className='commonButton'> Reset </Button>
+                                </Form.Item>
+                                <Form.Item className='commonItem'>
+                                    <Row>
+                                        <Col span={ 12 } className='commonGridComponent'>
+                                            <Button type='link' href='/login'> Login Here! </Button>
+                                        </Col>
+                                        <Col span={ 12 } className='commonGridComponent'>
+                                            <Button type='link' href='/register'> Or Register Here! </Button>
+                                        </Col>
+                                    </Row>
+                                </Form.Item>
+                            </Form>
+                        </Col>
+                        <Col xs={ 2 } sm={ 3 } md={ 6 } lg={ 7 } xl={ 8 } xxl={ 8 } />
+                    </Row>
+                </Content>
+            </Layout>
         );
     }
 }
 
 
-export default withStyles(styles, {withTheme: true})(Reset);
+export default Reset;

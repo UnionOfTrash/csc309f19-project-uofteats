995fc019a260ff5ec6417c2c8a3a4fda9b4c42d5
kexinlin <kexin.lin@mail.utoronto.ca>
2019-11-01 21:59:43 -0400

    fix: remove add item button from schedule page

--- src/customer/schedule_page/SchedulePage.js
+++ src/customer/schedule_page/SchedulePage.js
@@ -2,8 +2,20 @@ import React from "react";
 
 import HeaderBar from "../../commons/HeaderBar";
 import TruckHeader from "../food_page/TruckHeader";
-import { Typography, DatePicker, TimePicker, Form, Card, Input, Button, Modal, Alert, notification, Result } from 'antd';
-import moment from 'moment';
+import {
+  Typography,
+  DatePicker,
+  TimePicker,
+  Form,
+  Card,
+  Input,
+  Button,
+  Modal,
+  Alert,
+  notification,
+  Result
+} from "antd";
+import moment from "moment";
 
 const { Title } = Typography;
 class Schedule extends React.Component {
@@ -17,77 +29,74 @@ class Schedule extends React.Component {
       serveTime: "9:00 AM - 9:00 PM"
     },
     drawerVisible: false,
-    foodList: this.props.history.location.state && this.props.history.location.state.foodList || [],
+    foodList:
+      (this.props.history.location.state &&
+        this.props.history.location.state.foodList) ||
+      [],
     pickupTime: null,
     pickupDate: null,
     result: false
   };
 
   componentDidMount() {
-    console.log(this.props.history)
+    console.log(this.props.history);
   }
 
   get cartFoodPrice() {
-    let foodPrice = 0
+    let foodPrice = 0;
     this.state.foodList.forEach(category => {
       category.foods.forEach(food => {
         if (food.num) {
-          foodPrice = foodPrice + food.num * food.price.slice(1)
+          foodPrice = foodPrice + food.num * food.price.slice(1);
         }
-      })
-    })
-    return foodPrice
+      });
+    });
+    return foodPrice;
   }
 
   get cartFoodNum() {
-    let foodNum = 0
+    let foodNum = 0;
     this.state.foodList.forEach(category => {
       category.foods.forEach(food => {
         if (food.num) {
-          foodNum = foodNum + food.num
+          foodNum = foodNum + food.num;
         }
-      })
-    })
-    return foodNum
-  }
-
-  backToCart() {
-    this.props.history.goBack()
+      });
+    });
+    return foodNum;
   }
 
   setDate(pickupDate) {
     this.setState({
       pickupDate: pickupDate
-    })
+    });
   }
 
   setTime(pickupTime) {
     this.setState({
       pickupTime: pickupTime
-    })
+    });
   }
 
   pickup() {
     if (this.state.pickupDate && this.state.pickupTime) {
       this.setState({
         result: true
-      })
+      });
     } else {
       notification.config({
-        placement: 'bottomRight',
+        placement: "bottomRight"
       });
       notification.open({
-        message: 'Tips',
-        description:
-          'Please choose a pick-up time.',
+        message: "Tips",
+        description: "Please choose a pick-up time."
       });
     }
   }
   render() {
     return (
       <div>
-        {
-          !this.state.result &&
+        {!this.state.result && (
           <>
             <HeaderBar title="UofT Eats" username={this.state.userName} />
 
@@ -101,66 +110,93 @@ class Schedule extends React.Component {
               showCartDrawer={() => this.showCartDrawer()}
             />
             <br></br>
-            <Title level={2} style={{ paddingLeft: 20 }}>{this.state.userName}</Title>
+            <Title level={2} style={{ paddingLeft: 20 }}>
+              {this.state.userName}
+            </Title>
 
-            <Card title={`Items (${this.cartFoodNum})`} extra={<a onClick={() => this.backToCart()} style={{ color: '#1890ff' }}>Add Items</a>} style={{ margin: 20 }}>
+            <Card title={`Items (${this.cartFoodNum})`} style={{ margin: 20 }}>
               {this.state.foodList.map(category => {
-                if (category.foods.find(food => {
-                  return food.num
-                })) {
+                if (
+                  category.foods.find(food => {
+                    return food.num;
+                  })
+                ) {
                   return (
                     <>
-                      <Card title={category.category} key={category.category} hoverable>
+                      <Card
+                        title={category.category}
+                        key={category.category}
+                        hoverable
+                      >
                         {category.foods.map(food => {
                           if (food.num) {
-                            return <p key={food.id}>{food.name} * {food.num}</p>
+                            return (
+                              <p key={food.id}>
+                                {food.name} * {food.num}
+                              </p>
+                            );
                           } else {
-                            return null
+                            return null;
                           }
                         })}
                       </Card>
                       <br></br>
                     </>
-                  )
+                  );
                 } else {
-                  return null
+                  return null;
                 }
               })}
               <hr></hr>
-              <Title level={4} style={{ paddingLeft: 20 }}>Total： ${this.cartFoodPrice.toFixed(2)}</Title>
+              <Title level={4} style={{ paddingLeft: 20 }}>
+                Total： ${this.cartFoodPrice.toFixed(2)}
+              </Title>
             </Card>
             <Card title={`Schedule Pickup Time`} style={{ margin: 20 }}>
-
-              <Form layout='vertical'>
+              <Form layout="vertical">
                 <Form.Item label="Date">
-                  <DatePicker value={this.state.pickupDate} onChange={(value) => this.setDate(value)} />
+                  <DatePicker
+                    value={this.state.pickupDate}
+                    onChange={value => this.setDate(value)}
+                  />
                 </Form.Item>
                 <Form.Item label="Time">
-                  <TimePicker value={this.state.pickupTime} onChange={(value) => this.setTime(value)} format="HH:mm" />
+                  <TimePicker
+                    value={this.state.pickupTime}
+                    onChange={value => this.setTime(value)}
+                    format="HH:mm"
+                  />
                 </Form.Item>
                 <Form.Item>
                   <Input placeholder="Leave a note for the food truck" />
                 </Form.Item>
-                <Button type="primary" onClick={() => this.pickup()}>Schedule Pickup</Button>
+                <Button type="primary" onClick={() => this.pickup()}>
+                  Schedule Pickup
+                </Button>
               </Form>
             </Card>
           </>
-        }
-        {
-          this.state.result &&
+        )}
+        {this.state.result && (
           <Result
             status="success"
             title="Your pickup was successfully scheduled!"
-            subTitle={`Date:${moment(this.state.pickupDate).format('YYYY/MM/DD')} Time:${moment(this.state.pickupTime, 'HH:mm').format('HH:mm')}`}
+            subTitle={`Date:${moment(this.state.pickupDate).format(
+              "YYYY/MM/DD"
+            )} Time:${moment(this.state.pickupTime, "HH:mm").format("HH:mm")}`}
             extra={[
-              <Button type="primary" key="console" onClick={() => {
-                this.props.history.push('/')
-              }}>
+              <Button
+                type="primary"
+                key="console"
+                onClick={() => {
+                  this.props.history.push("/");
+                }}
+              >
                 Back to Home Page
               </Button>
             ]}
           />
-        }
+        )}
       </div>
     );
   }

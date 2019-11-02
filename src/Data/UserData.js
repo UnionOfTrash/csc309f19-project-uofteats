ceea8caada441d3d182b9c12103a0b62019260c7
zhang <yil.zhang@mail.utoronto.ca>
2019-11-01 21:57:15 -0400

    user profile, include checking history

--- src/Data/UserData.js
+++ src/Data/UserData.js
@@ -5,10 +5,14 @@ const userData = [
         password: '123456',
         image: UserImage,
         name: 'Tom',
-        phone: 123-456-7890,
+        phone: '123-456-7890',
         email: 'tom@mail.utoronto.ca',
         address: "",
         creditScore: 100,
+        history: [{
+            date: '2019-10-31',
+            order: ['All Beef HotDog', 'Chicken HotDog', 'Green Tea']
+        }]
     }
 ];
 

31d040a61e836dc1a75078929e28dbeac0e0f99f
kexinlin <kexin.lin@mail.utoronto.ca>
2019-11-22 17:49:28 -0500

    restructure to separate server and client side, add dependency: nodemon and concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 // Import BS4
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 
 // Import Routing Paths
@@ -17,25 +17,42 @@ import FoodPage from "./customer/food_page/FoodPage";
 import SchedulePage from "./customer/schedule_page/SchedulePage";
 import CustomerMain from "./customer/main_page/CustomerMain";
 import TruckView from "./Trucks/TruckView";
-import Admin from './admin/Admin'
+import Admin from "./admin/Admin";
 import UserProfileMain from "./customer/profile_page/UserProfileMain";
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <a href="/api/getList">Test</a>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/reset' component={Reset} />
-            <Route exact path="/customer/food_page/FoodPage" component={FoodPage} />
-            <Route exact path="/customer/schedule_page/SchedulePage" component={SchedulePage} />
-            <Route exact path="/customer/main_page/CustomerMain" component={CustomerMain}/>
-            <Route exact path='/t' component={TruckView} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/reset" component={Reset} />
+            <Route
+              exact
+              path="/customer/food_page/FoodPage"
+              component={FoodPage}
+            />
+            <Route
+              exact
+              path="/customer/schedule_page/SchedulePage"
+              component={SchedulePage}
+            />
+            <Route
+              exact
+              path="/customer/main_page/CustomerMain"
+              component={CustomerMain}
+            />
+            <Route exact path="/t" component={TruckView} />
             <Route exact path="/admin" component={Admin} />
-            <Route exact path="/customer/profile_page/UserProfileMain" component={UserProfileMain}/>
+            <Route
+              exact
+              path="/customer/profile_page/UserProfileMain"
+              component={UserProfileMain}
+            />
           </Switch>
         </BrowserRouter>
       </div>

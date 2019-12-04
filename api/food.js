7246449780e4fe8bf810a8eab74bc6b05efd4583
kexinlin <kexin.lin@mail.utoronto.ca>
2019-12-03 20:51:26 -0500

    fix bugs for model conflict and initial db

--- api/food.js
+++ api/food.js
@@ -2,7 +2,7 @@
 const log = console.log;
 
 const { ObjectID } = require("mongodb");
-const { Food } = require("../models/Food");
+const { Food } = require("../models/food");
 
 const getFood = (req, res) => {
   const id = req.params.id;
@@ -11,29 +11,31 @@ const getFood = (req, res) => {
     res.status(404).send();
   }
 
-  Food.findById(id).then((food) => {
-    if (!food) {
-      res.status(404).send();
-    } else {
-      res.send(food);
+  Food.findById(id).then(
+    food => {
+      if (!food) {
+        res.status(404).send();
+      } else {
+        res.send(food);
+      }
+    },
+    err => {
+      res.status(500).send(err);
     }
-  }, (err) => {
-    res.status(500).send(err);
-  });
-}
+  );
+};
 
 const addFood = (req, res) => {
-
-  const cata = req.body.category
+  const cata = req.body.category;
   let fimg;
-  if (cata === "HotDog"){
-    fimg = "./ChickenHotDog.jpeg"
-  }else if(cata === "Drink"){
-    fimg = "./Water.jpeg"
-  }else if(cata == "Poutine"){
-    fimg = "./Poutine.jpg"
-  }else{
-    fimg = "./star.png"
+  if (cata === "HotDog") {
+    fimg = "./ChickenHotDog.jpeg";
+  } else if (cata === "Drink") {
+    fimg = "./Water.jpeg";
+  } else if (cata == "Poutine") {
+    fimg = "./Poutine.jpg";
+  } else {
+    fimg = "./star.png";
   }
 
   const food = new Food({
@@ -41,15 +43,18 @@ const addFood = (req, res) => {
     name: req.body.name,
     price: req.body.price,
     category: cata,
-    img: fimg,
+    img: fimg
   });
 
-  food.save().then((result) => {
-    res.send(result);
-  }, (err) => {
-    res.status(500).send(err);
-  });
-}
+  food.save().then(
+    result => {
+      res.send(result);
+    },
+    err => {
+      res.status(500).send(err);
+    }
+  );
+};
 
 const modifyFood = (req, res) => {
   const id = req.params.id;
@@ -62,20 +67,22 @@ const modifyFood = (req, res) => {
     name: req.body.name,
     price: req.body.price,
     category: req.body.category,
-    img: req.body.img,
+    img: req.body.img
   };
 
-  Food.findByIdAndUpdate(id, { $set: newFood }, { new: true })
-    .then((result) => {
+  Food.findByIdAndUpdate(id, { $set: newFood }, { new: true }).then(
+    result => {
       if (!result) {
         res.status(404).send();
       } else {
         res.send(result);
       }
-    }, (err) => {
+    },
+    err => {
       res.status(500).send(err);
-    });
-}
+    }
+  );
+};
 
 const deleteFood = (req, res) => {
   const id = req.params.id;
@@ -84,33 +91,39 @@ const deleteFood = (req, res) => {
     res.status(404).send();
   }
 
-  Food.findByIdAndRemove(id).then((result) => {
-    if (!result) {
-      res.status(404).send();
-    } else {
-      res.send(result);
+  Food.findByIdAndRemove(id).then(
+    result => {
+      if (!result) {
+        res.status(404).send();
+      } else {
+        res.send(result);
+      }
+    },
+    err => {
+      res.status(500).send(err);
     }
-  }, (err) => {
-    res.status(500).send(err);
-  });
-}
+  );
+};
 
 const getFoodsByTruck = (req, res) => {
-    const truckId = req.params.truckId;
+  const truckId = req.params.truckId;
 
-    if (!ObjectID.isValid(truckId)) {
+  if (!ObjectID.isValid(truckId)) {
+    res.status(404).send();
+  }
+
+  Food.find({ truckId: truckId }).then(
+    result => {
+      if (!result) {
         res.status(404).send();
-    }
-
-    Food.find({ truckId: truckId }).then((result) => {
-       if (!result) {
-         res.status(404).send();
-       } else {
-         res.send(result);
-       }
-    }, (err) => {
+      } else {
+        res.send(result);
+      }
+    },
+    err => {
       res.status(500).send(err);
-    });
-}
+    }
+  );
+};
 
 module.exports = { getFood, addFood, modifyFood, deleteFood, getFoodsByTruck };
---
+++ client/src/data/TrackData.js
@@ -0,0 +1,106 @@
+import TruckImg from "../customer/images/truck1.png";
+
+const trackData = [
+  {
+    id: 1,
+    name: "truck 1",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 1",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 2,
+    name: "truck 2",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 2",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 3,
+    name: "truck 3",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 3",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 4,
+    name: "truck 4",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 4",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 5,
+    name: "truck 5",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 3",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 6,
+    name: "truck 6",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 3",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 7,
+    name: "truck 7",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 3",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 8,
+    name: "truck 8",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 3",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 9,
+    name: "truck 9",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 3",
+    foodPage: "../food_page/FoodPage"
+  },
+  {
+    id: 10,
+    name: "truck 10",
+    location: "10 st George street",
+    type: "Asian",
+    rate: 4.0,
+    image: TruckImg,
+    alternative: "truck 3",
+    foodPage: "../food_page/FoodPage"
+  }
+];
+
+export default trackData;

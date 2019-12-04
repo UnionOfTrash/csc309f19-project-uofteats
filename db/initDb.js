b4c5088c7cedfd441105bfc0ede8d0b6f3a35ea7
zhang <yil.zhang@mail.utoronto.ca>
2019-12-03 20:43:51 -0500

    initDb

--- db/initDb.js
+++ db/initDb.js
@@ -3,12 +3,8 @@ const mongoose = require("mongoose");
 const { UserAuth } = require("../models/UserAuth");
 const { Student } = require("../models/Student");
 const { Truck } = require("../models/Truck");
-<<<<<<< HEAD
 const { Food } = require("../models/food");
 const { Order } = require("../models/order");
-=======
-const { Food } = require("../models/Food");
->>>>>>> ff2b5b090a6d02a088f5bc123d9e6e6315c4bc92
 
 // Add a new admin
 const admin = new UserAuth({
@@ -67,7 +63,6 @@ studentAuth.save().then(
 const truckAuth = new UserAuth({
   username: "user1",
   password: "user1",
-<<<<<<< HEAD
   role: "Truck"
 });
 
@@ -95,11 +90,10 @@ truckAuth.save().then(
         log(err);
       }
     );
-  },
-  err => {
-=======
-  role: "Truck",
-});
+  }, err => {
+      log(err);
+  }
+);
 
 truckAuth.save().then((res0) => {
   const truck = new Truck({
@@ -139,7 +133,6 @@ truckAuth.save().then((res0) => {
     italianSpicySausage.save().then((err) => {
       if (err) log(err);
     });
-
     const germanSausage = new Food({
       truckId: res1._id,
       name: "German Sausage",
@@ -161,7 +154,6 @@ truckAuth.save().then((res0) => {
     chickenHotdog.save().then((err) => {
       if (err) log(err);
     });
-
     const frenchFries = new Food({
       truckId: res1._id,
       name: "French Fries",
@@ -183,7 +175,6 @@ truckAuth.save().then((res0) => {
     chickenNuggets.save().then((err) => {
       if (err) log(err);
     });
-
     const onionRings = new Food({
       truckId: res1._id,
       name: "Onion Rings",
@@ -217,134 +208,7 @@ truckAuth.save().then((res0) => {
       if (err) log(err);
     });
   }, (err) => {
->>>>>>> ff2b5b090a6d02a088f5bc123d9e6e6315c4bc92
     log("Error when adding a truck");
     log(err);
-  }
-);
-
-// initialize some food
-const beefHotdog = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Hot Dogs",
-  name: "All Beef Hotdog",
-  price: 4.0,
-  img: "./BeefHotDog.jpeg"
-});
-beefHotdog.save(function(err) {
-  if (err) console.log(err);
-});
-
-const italianSpicySausage = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Hot Dogs",
-  name: "Italian Spicy Sausage",
-  price: 4.0,
-  img: "./ItalianSpicySausage.jpeg"
-});
-italianSpicySausage.save(function(err) {
-  if (err) console.log(err);
-});
-
-const germanSausage = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Hot Dogs",
-  name: "German Sausage",
-  price: 4.0,
-  img: "./GermanSausage.jpeg"
-});
-germanSausage.save(function(err) {
-  if (err) console.log(err);
-});
-
-const chickenHotdog = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Hot Dogs",
-  name: "Chicken Hotdog",
-  price: 4.0,
-  img: "./ChickenHotDog.jpeg"
-});
-chickenHotdog.save(function(err) {
-  if (err) console.log(err);
-});
-
-const frenchFries = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Sides",
-  name: "French Fries",
-  price: 2.5,
-  img: "./FrenchFries.jpeg"
-});
-frenchFries.save(function(err) {
-  if (err) console.log(err);
-});
-
-const chickenNuggets = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Sides",
-  name: "Chicken Nuggets",
-  price: 4.5,
-  img: "./ChickenNuggets.jpg"
-});
-chickenNuggets.save(function(err) {
-  if (err) console.log(err);
-});
-
-const onionRings = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Sides",
-  name: "Onion Rings",
-  price: 4.5,
-  img: "./OnionRings.jpg"
-});
-onionRings.save(function(err) {
-  if (err) console.log(err);
-});
-
-const canadaDry = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Beverages",
-  name: "Canada Dry",
-  price: 1.25,
-  img: "./CanadaDry.jpg"
-});
-canadaDry.save(function(err) {
-  if (err) console.log(err);
-});
-
-const greenTea = new Food({
-  _id: new mongoose.Types.ObjectId(),
-  truckId: truckAuth._id,
-  category: "Beverages",
-  name: "Green Tea",
-  price: 1.25,
-  img: "./GreenTea.jpg"
-});
-greenTea.save(function(err) {
-  if (err) console.log(err);
-});
-
-const order1 = new Order({
-  _id: new mongoose.Types.ObjectId(),
-  customerId: studentAuth._id,
-  truckId: truckAuth._id,
-  food: [
-    { foodId: beefHotdog._id, quantity: 1 },
-    { foodId: canadaDry._id, quantity: 2 }
-  ],
-  price: beefHotdog.price * 1 + canadaDry.price * 2,
-  pickDate: "2019/12/27",
-  pickTime: "22:10",
-  noteContent: "Extra ketchup, thanks"
-});
-order1.save(function(err) {
-  if (err) console.log(err);
+  });
 });
